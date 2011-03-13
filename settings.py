9b8beaea02221a1396f60a7e9b13d469e49ad4ed
Jeremy Lainé <jeremy.laine@m4x.org>
2011-03-13 00:02:47 +0000

    remove reference to flatpages
    git-svn-id: svn+ssh://svn.bolloretelecom.eu/home/svn/opensource/django-granadilla/trunk@980 e071eeec-0327-468d-9b6a-08194a12b294

--- settings.py
+++ settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
